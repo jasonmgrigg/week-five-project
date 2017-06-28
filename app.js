4a6bc33df84205e566903e9ae0b4281790d898ad
Jason Grigg <boatdreamin@gmail.com>
2017-06-28 12:57:40 -0400

    file creation

--- app.js
+++ app.js
@@ -1,88 +1,138 @@
-// Require dependencies.
 const express = require('express');
-const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
-const session = require('express-session')
-const parseurl = require('parseurl')
-
-// Create app.
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const sessionConfig = require("./sessionCounter");
+//needed for file-system to work
+const fs = require('file-system');
 const app = express();
-// Instantiate letterList array.
-const letterList = [];
-// Configure dependencies.
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
-app.set('view engine', 'mustache');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-app.use(express.static('./public'));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(function (req, res, next) {
-  var views = req.session.views
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-  if (!views) {
-    views = req.session.views = {}
+//Functions to get words from local file.
+
+function getWord() {
+  let randomWord;
+  let wordLength = 0;
+  let wordFound = false;
+
+  while (!wordFound) {
+    //Random word generator
+    let randomNumber = Math.floor((Math.random() * words.length-1) + 1)
+    randomWord = words[randomNumber];
+    wordLength = randomWord.length;
+          wordFound = true;
+          break;
   }
+  console.log(randomWord);
+  //returns random word that was chosen
+  return randomWord.toUpperCase();  //changing word to upper case
+};
 
+function play(game) {
+  var showText = [];
+  //starts the game playing
+  for (let i = 0; i < game.word.length; i++) {  //function to cycle through word array to find word.
+    if (game.lettersGuessed.indexOf(game.word[i]) > -1) {
+       showText.push(game.word[i].toUpperCase());  //pushes the game.word(word randomly chosen), into the showText array
+     } else {
+       if (game.lose == true) {
+          showText.push(game.word[i].toUpperCase()); //displays letters in boxes, if lose game display all.
+          console.log("showText is " + showText);
+       } else {
+          showText.push(' '); //pushes empty array of characters into the showText array
+       }
+     }
+  }
+  console.log(showText)
+  return showText;
+};
+//End Functions
 
-  // get the url pathname
-    var pathname = parseurl(req).pathname
+app.engine("mustache", mustacheExpress());
+app.set("views", "./public");
+app.set("view engine", "mustache");
 
-    // count the views
-    views[pathname] = (views[pathname] || 0) + 1
+app.use("/", express.static("./public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+app.use(session(sessionConfig));  //found code to make this work, still not sure why I had to have it
 
-    next()
-    console.log(views)
-  })
-
-  app.get('/', function (req, res, next) {
-    // res.send('you viewed this page ' + req.session.views['/'] + ' times')
-    // res.render('index', {views: views});
-//   })
-//
-//   app.get('/bar', function (req, res, next) {
-//     res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
-//   })
-//
-// // Add route to render the To Do list on the index page.
-// app.get('/', function(req, res){
-  res.render('index', {letterList});
+app.use(function (req, res, next) {  //sending code to mustache file to render on display
+  var game = req.session.game;
+  console.log(game);  //shows the below variables and how they are defined in the beginning of the game.
+  if (!game) {
+    game = req.session.game = {};  //empty array
+    game.guessesLeft = 8;  //chances left to guess a letter
+    game.lettersGuessed = [];  //this is an empty array since nothing has been guessed at this point.
+    game.btnText = 'Play game';  //text that is dislayed in the button
+    game.status = '';  //game status display, which is nothing at this time
+    game.lose = false;  //boolean to see if game has been lost to trigger another action
+    game.playing = false;
+    game.display = '';
+  }
+  next();
 });
 
-// Receives data from form (action='/')
-// 'req.body' now contains form data.
-app.post('/', function(req, res){
-// Check for validation errors and create error message.
-  req.checkBody('item', 'Please enter something to do.' ).notEmpty();
+app.get('/', function(req, res) {
+  if (req.session.game.playing || req.session.game.btnText != 'Play game') {
+    req.session.game.display = play(req.session.game);
+  }
+  res.render('index', { game: req.session.game });
+});
 
-  let errors = req.validationErrors();
-  if (errors) {
-// Render validation error messages.
-    res.render('index', {errors: errors});
-  } else {
-// Add new letter to the letter list.
-      let letter = {
-        'item': req.body.item,
-        // 'priority': req.body.priority,
-        // 'checked': ""
+app.post('/', function(req, res) {
+  var game = req.session.game;
+  if (game.playing) {
+    req.checkBody("guessLetter", "You must enter a letter!").notEmpty().isAlpha();
+    var errors = req.validationErrors();
+    if (errors) {
+      game.message = errors[0].msg;
+    } else {
+      if (game.lettersGuessed.indexOf(req.body.guessLetter.toUpperCase()) > -1) {
+        game.message = 'You already guessed letter ' + req.body.guessLetter.toUpperCase();;
+      } else {
+        var n = game.word.indexOf(req.body.guessLetter.toUpperCase());
+        if (n == -1) {
+          game.message = 'Bad guess...try again!';
+          game.guessesLeft -= 1;
+          game.lettersGuessed.push(req.body.guessLetter.toUpperCase());
+          if (game.guessesLeft == 0) {
+            game.btnText = 'Try again';
+            game.status = 'You lose!';
+            game.playing = false;
+            game.lose = true;
+          }
+        } else {
+          game.lettersGuessed.push(req.body.guessLetter.toUpperCase());
+          game.message = '';
+          // check for win ---------------------------
+          req.session.game.display = play(req.session.game);
+          if (game.display.indexOf(' ') ==  -1) {
+            game.btnText = 'Try again';
+            game.status = 'You win!';
+            game.playing = false;
+            game.lose = false;
+          }
+        }
       }
-      // let views = {
-      //   'view': req.body.views
-      // }
-      letterList.push(letter);
-      // viewsList.push(views);
-      console.log(letterList);
-// Render index page with updated To Do list.
-      res.render('index', {letterList});
     }
-  });
-// Local web server to run the app.
+  } else {
+    game.playing = true;
+    game.btnText = "Make a guess";
+    game.word = getWord(game.mode);
+    game.lose = false;
+    game.guessesLeft = 8;
+    game.lettersGuessed = [];
+  }
+
+  res.redirect('/');
+});
+
+
+
+
+
 app.listen(3000, function(){
   console.log('Started express application!')
 });
